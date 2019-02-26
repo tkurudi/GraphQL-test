9a341e1d3474bdb017351e5e9de53d96d42dcc86
tkurudi <t.kurudi209@gmail.com>
2019-02-26 17:16:32 +1300

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
-import logo from "./logo.png";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import "./App.css";
-import Launches from "./components/Launches";
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+import './App.css';
+import logo from './logo.png';
+
+
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -13,14 +17,17 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
+      <Router>
         <div className="container">
           <img
             src={logo}
             alt="SpaceX"
             style={{ width: 300, display: "block", margin: "auto" }}
           />
-          <Launches />
+         <Route exact path="/" component={Launches} />
+         <Route exact path="/launch/:flight_number" component={Launch} />
         </div>
+        </Router>
       </ApolloProvider>
     );
   }
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
+import MissionKey from './MissionKey';
 
 
 const LAUNCHES_QUERY = gql`
@@ -20,6 +21,7 @@ export default class Launches extends Component {
     return (
       <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey/>
         <Query query={LAUNCHES_QUERY}>
         {
             ({ loading, error, data }) => {
